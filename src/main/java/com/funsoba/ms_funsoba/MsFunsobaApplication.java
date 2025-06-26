e72835abfec880e04ad05d542d7f1899ec05b56c
Yusly Arboleda <yuslyarboleda1@gmail.com>
2025-06-26 15:49:09 -0500

    creación del backend

---
+++ src/main/java/com/funsoba/ms_funsoba/MsFunsobaApplication.java
@@ -0,0 +1,13 @@
+package com.funsoba.ms_funsoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFunsobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFunsobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funsoba/ms_funsoba/MsFunsobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funsoba.ms_funsoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFunsobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
